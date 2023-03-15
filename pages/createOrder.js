6df675146cf802287b6b9ba9f5dbee2cc4b3221e
Maddi Cook <maddihcook@gmail.com>
2023-03-14 21:29:15 -0500

    create order form

---
+++ pages/createOrder.js
@@ -0,0 +1,33 @@
+import renderToDOM from '../utils/renderToDom';
+
+const createOrderPage = () => {
+  let domString = '';
+  domString += `  
+  <form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Order Name</label>
+      <input type="text" class="form-control" id="ordername" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Customer Phone</label>
+      <input type="text" class="form-control" id="custphone">
+    </div>
+    <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Customer Email</label>
+    <input type="text" class="form-control" id="custemail">
+  </div>
+  <div class="dropdown-center">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Order Type
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">In-Person</a></li>
+    <li><a class="dropdown-item" href="#">Phone</a></li>
+  </ul>
+</div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>`;
+  renderToDOM('#main-container', domString);
+}
+
+export default createOrderPage;
---
+++ pages/welcome.js
@@ -0,0 +1,22 @@
+import loginButton from '../src/scripts/components/loginButton';
+import renderToDOM from '../utils/renderToDom';
+import logo from '../instructions/hhpw-record.png';
+
+const welcomePage = () => {
+  document.querySelector('#app').innerHTML = '';
+  const domString = `
+      <div>
+      <div id="text-style">
+      <h1 id="text-styles"> Welcome to Hip-Hop Pizza and Wangs</h1>
+      <img src=${logo} alt="store logo">
+      <div id="login-button"><div>
+      </div>
+      <br>
+      </div>
+  
+      `;
+  renderToDOM('#app', domString);
+  loginButton();
+};
+
+export default welcomePage;
